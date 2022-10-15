5fb72a4951c59c3fa4eada7d27d236ef1313d78d
Morné Louw <morne.louw@stepstone.com>
2022-10-15 18:35:25 +0200

    chore: remove the logRoles demo code

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,8 @@
-import { render, screen, logRoles } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
-  // If you don't know what the available roles are that you can query for RTL comes with a handy debugging tool to help you determine the available roles
-  // logRoles() - imported from @testing-library/react (in the tutorial it still uses the deprecated import from @testing-library/dom)
-
-  // Destructure the render response into a container object
-  const { container } = render(<App />);
-
-  // now take a peek at the available roles on this container
-  logRoles(container);
+  render(<App />);
 
   // find an element with the role of button and the test (a11y-label for buttons) of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
