066b22e202d98a51b4fc54343cfcafa0c4742d25
Morné Louw <morne.louw@stepstone.com>
2022-10-15 17:48:18 +0200

    test: tdd step 3 - develop the code to make the test pass

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
-  render('App/>');
+  render(<App />);
   // find an element with the role of button and the test (a11y-label for buttons) of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
   // Possible custom matchers are listed here: https://github.com/testing-library/jest-dom
   // We wan to test the initial background color for the button and expect it to be red
-  expect(colorButton).toHaveStyle({ backgoundColor: 'red' });
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
 
 test.skip('button turns blue when clicked', () => {
