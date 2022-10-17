bae6d76ce287bb5a87d66b95adf0ab21dcb717dd
monello <louw.morne@gmail.com>
2022-10-17 22:03:22 +0200

    feat: add the skeleton code for options and scoopoptions

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
 
 const Options = ({ optionType }) => {
-    return <div>Options</div>;
+    const [items, setItems] = useState([]);
+
+    // optionType is 'scoops' or 'toppings'
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3030/${optionType}`)
+            .then((response) => setItems(response.data))
+            .catch((error) => {
+                // TODO: handle error response
+            });
+    }, [optionType]);
+
+    // TODO: replace 'null' woth ToppingOption once it is available
+    const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+    const optionItems = items.map((item) => (
+        <ItemComponent
+            key={item.name}
+            name={item.name}
+            imagePath={item.imagePath}
+        />
+    ));
+
+    return <Row>{optionItems}</Row>;
 };
 
 export default Options;
