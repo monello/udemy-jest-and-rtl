5199efba0ff2d5c626e97be8ac4052febea1a954
monello <louw.morne@gmail.com>
2022-10-17 22:09:31 +0200

    test: turn the test into an async-await test due to promise beig returned from axios

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -3,11 +3,13 @@ import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 describe("Options component", () => {
-    it("displays image for each scoop from server", () => {
+    it("displays image for each scoop from server", async () => {
         render(<Options optionType="scoops" />);
 
         // find images
-        const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+        const scoopImages = await screen.findAllByRole("img", {
+            name: /scoop$/i,
+        });
         expect(scoopImages).toHaveLength(2);
 
         // confirmm alt test of images
