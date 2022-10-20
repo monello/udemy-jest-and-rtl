ea83d7803eb176540164d89b34879d3ad52c073f
monello <louw.morne@gmail.com>
2022-10-20 08:40:03 +0200

    test: testing the sub-totals checkboxes

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -12,7 +12,7 @@ const ScoopOption = ({ name, imagePath }) => {
         <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
             <img
                 style={{ width: "75%" }}
-                src={`http://localhost:3030:${imagePath}`}
+                src={`http://localhost:3030${imagePath}`}
                 alt={`${name} scoop`}
             />
             <Form.Group
